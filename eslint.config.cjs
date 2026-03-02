0b2eb5e7a68dbc2aaa8b10058150c4dd6df05a3d
Max R <mxr@users.noreply.github.com>
2026-03-01 21:55:58 -0500

    Use @eslint/js recommended config

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,15 +1,17 @@
+const js = require("@eslint/js");
+const globals = require("globals");
+
 module.exports = [
-  {
-    files: ["scripts/**/*.js"],
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "script",
+    js.configs.recommended,
+    {
+        files: ["scripts/**/*.js"],
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "script",
+            globals: {
+                ...globals.browser,
+                ...globals.es2021,
+            },
+        },
     },
-    rules: {
-      indent: ["error", 4],
-      quotes: ["error", "double", { avoidEscape: true }],
-      semi: ["error", "always"],
-      "comma-dangle": ["error", "always-multiline"],
-    },
-  },
 ];
