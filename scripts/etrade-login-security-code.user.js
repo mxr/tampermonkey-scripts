8ff8f28ebbba245459bd555ad16da5866cf70525
Max R <mxr@users.noreply.github.com>
2026-05-18 19:18:49 -0400

    Bump version to 1.0.1

--- scripts/etrade-login-security-code.user.js
+++ scripts/etrade-login-security-code.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         E*TRADE Login: Enable Use Security Code
 // @namespace    https://github.com/mxr/tampermonkey-scripts
-// @version      1.0.0
+// @version      1.0.1
 // @description  Automatically checks the "Use security code" checkbox on E*TRADE login.
 // @author       mxr
 // @match        https://us.etrade.com/home/welcome-back*
